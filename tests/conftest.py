a7c0dcbc0ae5c76a56ebd91492fb932e7458152a
Weder Videira Borges <wederborgesemp@gmail.com>
2026-04-25 13:31:08 -0400

    Testes, alembic e migrations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,47 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest  # arquivo de configuração dos testes
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from fast_api_zero.app import app  # importando a var app.
+from fast_api_zero.models import User, table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _moc_db_time(*, model, time=datetime(2025, 5, 26)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'created_at'):
+            target.update_at = time
+
+    event.listen(User, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(User, 'before_inser', fake_time_hook)
+
+
+@pytest.fixture
+def moc_db_time():
+    return _moc_db_time
---
+++ tests/tests_db.py
@@ -0,0 +1,25 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_api_zero.models import User
+
+
+def test_create_user(session, moc_db_time):
+
+    with moc_db_time(model=User) as time:
+        new_user = User(username='weder', email='teste', password='secret')
+
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'teste'))
+
+        assert asdict(user) == {
+            'id': 1,
+            'username': 'teste',
+            'email': 'teste',
+            'password': 'secret',
+            'created_at': time,
+            'update_at': time,
+        }
